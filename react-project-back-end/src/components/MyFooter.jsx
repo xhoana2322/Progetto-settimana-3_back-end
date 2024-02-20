454300942361c00a9398b98a15826200d62c76b7
xhoana2322 <xhoana.beqiri23@gmail.com>
2024-02-20 12:13:21 +0100

    :D

--- react-project-back-end/src/components/MyFooter.jsx
+++ react-project-back-end/src/components/MyFooter.jsx
@@ -8,10 +8,10 @@ import img from '../assets/img/app_store_google_play.png'
 export default function MyFooter() {
   return (
     <>
-        <footer className='text-uppercase mt-4'>
+        <footer className='container text-uppercase mt-4'>
             <Row>
-                <Col className='d-flex'>
-                    <div className='d-flex flex-column'>
+                <Col md={5} className='d-flex lh-lg'>
+                    <div className='d-flex flex-column '>
                         <Link>allergen info</Link>
                         <Link>nutrition</Link>
                         <Link>faqs</Link>
@@ -26,24 +26,24 @@ export default function MyFooter() {
                         <Link>leadership</Link>
                     </div>
                 </Col>
-                <Col>
+                <Col md={4}>
                     <div className='d-flex'>
-                        <p>get the mod</p>
-                        <p className='ms-2'>rewards app</p>  
+                        <p className='mb-0'>get the mod</p>
+                        <p className='ms-2 mb-0'>rewards app</p>  
                     </div>
                     
                     <img src={img} alt="" width={140} height={90} />
                 </Col>
-                <Col>
-                <p>follow us</p>
-                <div className='d-flex'>
+                <Col md={2}>
+                <p className='mb-1'>follow us</p>
+                <div className='d-flex fs-3 text-danger'>
                     <i className="bi bi-facebook"></i>
                     <i className="bi bi-twitter mx-3"></i>
                     <i class="bi bi-instagram"></i>
                 </div>
                 </Col>
             </Row>
-            <div className='d-flex justify-content-between w-75 mt-4'>
+            <div className='d-flex justify-content-between mt-4'>
                 <p>© 2024 MOD super fast pizza, LLC.</p>
                 <Link>privacy</Link>
                 <Link>terms of use</Link>
